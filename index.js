dfe367f6cc66aa2dcc21e84ded37735d7d0e0371
Subin <sub_1n@yahoo.com>
2023-09-26 11:48:44 +0300

    html, js and css files

---
+++ index.js
@@ -0,0 +1,18 @@
+const textareaEl = document.getElementById("textarea");
+
+const totalCounterEl = document.getElementById("total-counter");
+
+const remCounterEl = document.getElementById("remaining-counter");
+
+textareaEl.addEventListener("keyup", () => {
+  updateCounter();
+});
+
+updateCounter();
+
+function updateCounter() {
+  totalCounterEl.innerText = textareaEl.value.length;
+
+  remCounterEl.innerText =
+    textareaEl.getAttribute("maxLength") - textareaEl.value.length;
+}
